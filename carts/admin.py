a570acb53333dd8ac8a949212d942cab5e4f9b29
Ishita Batra <ishitabatra@Ishitas-MacBook-Air.local>
2026-09-10 00:49:36 +0530

    store and carts functionality

---
+++ carts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Cart, CartItem  
+
+# Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'date_added')
+    list_per_page = 20
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
+    list_editable = ('quantity', 'is_active')
+    list_per_page = 20
+
+admin.site.register(Cart,CartAdmin)
+admin.site.register(CartItem,CartItemAdmin)
